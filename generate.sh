7e9fd9920d7e44840a1b754928f9975785938566
neeasade <nathanisom27@gmail.com>
2017-05-29 16:54:26 -0500

    debug, ship it.

--- generate.sh
+++ generate.sh
@@ -7,16 +7,16 @@ cd "$(dirname $([ -L $0  ] && readlink -f $0 || echo $0))"
 
 fill_templates() {
     IFS=$'\n'
-    for template in "$(find templates -name "*.*")"; do
+    for template in $(find templates -type f); do
         target="./output/$(echo ${template#*/})"
         target="$(dirname $target)/${1}.$(basename "$target")"
         mkdir -p "$(dirname "$target")"
-        mustache < "$template" > "$target"
+        mustache < "./$template" > "$target"
     done
     IFS=
 }
 
-for scheme in $(find schemes -name "*.*"); do
+for scheme in $(find schemes -type f); do
     . "$scheme"
     fill_templates "$(basename "$scheme")"
 done
